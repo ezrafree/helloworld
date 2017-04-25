84b61868db376081f7b8e0990e3eedd748d264e8
Ezra Free <ezrafree@gmail.com>
2017-04-25 16:13:15 -0600

    add c++ example

---
+++ c++/helloworld.cpp
@@ -0,0 +1,16 @@
+/**
+ * Hello World
+ *
+ * Compile with:
+ * $ g++ -o bin/helloworld helloworld.cpp
+ *
+ * Run with:
+ * $ ./bin/helloworld
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello, world\n";
+}
